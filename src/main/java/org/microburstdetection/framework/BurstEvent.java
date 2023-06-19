fbbe77d1d392e8fca860a0731bb852c14111ec5a
arshiya <arshia.rezaee@yahoo.com>
2023-06-19 13:04:23 +0330

    Burst Events are captured in a Record

---
+++ src/main/java/org/microburstdetection/framework/BurstEvent.java
@@ -0,0 +1,25 @@
+package org.microburstdetection.framework;
+
+import java.math.BigInteger;
+
+public record BurstEvent(int numberOfBurstyPackets, int traversedBytes, long arrivalTimeOfFirstPacket, long arrivalTimeOfLastPacket) {
+
+    public int getNumberOfPackets() {
+        return numberOfBurstyPackets;
+    }
+
+    public int getTraversedBytes() {
+        return traversedBytes;
+    }
+
+    public long getArrivalTimeOfFirstPacket() {
+        return arrivalTimeOfFirstPacket;
+    }
+
+    public long getArrivalTimeOfLastPacket() {
+        return arrivalTimeOfLastPacket;
+    }
+    public long getBurstDuration(){
+        return arrivalTimeOfLastPacket-arrivalTimeOfFirstPacket;
+    }
+}
